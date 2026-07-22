c27ad162db2698e328ee80d49e85a1fda5b4b72c
Shikha Yadav <shikhayadav1916@gmail.com>
2026-07-22 10:39:43 +0530

    Your commit message describing the changes

--- Carapp.py
+++ Carapp.py
@@ -15,38 +15,34 @@ def get_brand_name(car_name):
 cars_data['name'] = cars_data['name'].apply(get_brand_name)
 
 name = st.selectbox('Select Car Brand', cars_data['name'].unique())
-year = st.slider('Car Manufactured Year', 1994, 2026)
-km_driven = st.slider('No of kms Driven', 11, 200000)
+year = st.slider('Car Manufactured Year', 1994, 2026, value=1994)
+km_driven = st.slider('No of kms Driven', 11, 200000, value=11)
 fuel = st.selectbox('Fuel type', cars_data['fuel'].unique())
 seller_type = st.selectbox('Seller type', cars_data['seller_type'].unique())
 transmission = st.selectbox('Transmission type', cars_data['transmission'].unique())
-owner = st.selectbox('Owner type', cars_data['owner'].unique()) # Fixed the duplicate "Seller type" label here
-mileage = st.slider('Car Mileage', 10, 40)
-engine = st.slider('Engine CC', 700, 5000)
-max_power = st.slider('Max Power', 0, 200)
-seats = st.slider('No of Seats', 5, 10)
+owner = st.selectbox('Owner type', cars_data['owner'].unique())
+mileage = st.slider('Car Mileage', 10, 40, value=10)
+engine = st.slider('Engine CC', 700, 5000, value=700)
+max_power = st.slider('Max Power', 0, 200, value=0)
+seats = st.slider('No of Seats', 5, 10, value=5)
 
 if st.button("Predict"):
-
     brand_map = {val: i for i, val in enumerate(sorted(cars_data['name'].unique()))}
     fuel_map = {val: i for i, val in enumerate(sorted(cars_data['fuel'].unique()))}
     seller_map = {val: i for i, val in enumerate(sorted(cars_data['seller_type'].unique()))}
     trans_map = {val: i for i, val in enumerate(sorted(cars_data['transmission'].unique()))}
     owner_map = {val: i for i, val in enumerate(sorted(cars_data['owner'].unique()))}
-    
+
     mapped_name = brand_map[name]
     mapped_fuel = fuel_map[fuel]
     mapped_seller = seller_map[seller_type]
     mapped_trans = trans_map[transmission]
     mapped_owner = owner_map[owner]
-    
-    input_data_model = pd.DataFrame(
-        [[mapped_name, year, km_driven, mapped_fuel, mapped_seller, mapped_trans, mapped_owner, mileage, engine, max_power, seats]],
-        columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner', 'mileage', 'engine', 'max_power', 'seats']
-    )
 
-    car_price = model.predict(input_data_model)
-    
-    st.markdown(f'### Car Price is going to be ₹ {round(car_price[0], 2):,}')
+    input_data_model = pd.DataFrame([[mapped_name, year, km_driven, mapped_fuel, mapped_seller, mapped_trans, mapped_owner, mileage, engine, max_power, seats]],
+                                    columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner', 'mileage', 'engine', 'max_power', 'seats'])
 
+    log_price = model.predict(input_data_model)[0]
+    final_price = np.exp(log_price)
+    st.markdown(f'### Car Price is going to be ₹ {round(final_price, 2):,}')
     st.success("Car Price Predicted Successfully..")
